85e29bda0bd0416b1072a52147d1afd396808d7c
王冰 <wangbinge@seentao.com>
2020-11-13 21:48:28 +0800

    运行时配置

--- src/app.js
+++ src/app.js
@@ -6,3 +6,14 @@ export const dva = {
     },
   },
 };
+export function patchRoutes(routes) {
+  console.log('routes',routes)
+}
+// 延迟渲染，先做鉴权
+export function render(oldRender) {
+  setTimeout(oldRender, 1000);
+}
+// 可以用于做埋点
+export function onRouteChange({ location, routes, action }) {
+  console.log('loaction',location)
+}
