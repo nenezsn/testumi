1b41dbf6662b1d7fac9ba06c19b586ab2d233bdf
王冰 <wangbinge@seentao.com>
2020-11-14 19:20:52 +0800

    request增加错误提示

--- src/util/request.js
+++ src/util/request.js
@@ -8,17 +8,21 @@ const request = extend({
   prefix: "/test/",
   timeout: 1000, //超时取消处理
   headers: {
-    "Content-Type": "multipart/form-data"
+    'Content-Type': 'application/x-www-form-urlencoded'
   },
   errorHandler: function (error) {
-    console.log('error', error)
+    const { response={} } = error
+    if(response.status <200 ||
+      response.status > 300){
+        return {
+          code:response.status,
+          msg:'有问题接口:'+response.url+':'+response.statusText
+        }
+      }
   }
 });
 export default function(url,params={}){
   return request.post(url, {
-    body: querystring(params),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
+    body: querystring(params)
   })
 }
