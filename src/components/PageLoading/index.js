1c55cf5ab2847c6dc0c4e85c0c8044229ec18ac7
王冰 <wangbinge@seentao.com>
2020-12-14 09:09:54 +0800

    关闭git提交检查

--- src/components/PageLoading/index.js
+++ src/components/PageLoading/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Spin } from 'antd';
-console.log('2222')
 // loading components from code split
 // https://umijs.org/plugin/umi-plugin-react.html#dynamicimport
 export default () => (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
-import index from '@utils/index'
-import Child from '@components/child'
 function Index(props) {
-return <div>
+  return <div>
     <h3>首页</h3>
-    <Child/>    
     {props.children}</div>
 }
 export default Index
